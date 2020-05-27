70ada22ac44e64c3212e92bb3affe4915efd949a
vj_akash <48998005+vjakash@users.noreply.github.com>
2020-05-27 18:36:11 +0530

    form array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,105 +1,156 @@
 import { Component } from '@angular/core';
-import {FormGroup,FormControl, Validators} from '@angular/forms'
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder,
+  FormArray,
+} from '@angular/forms';
+import { stringify } from 'querystring';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'reactiveForms';
-  myForm:FormGroup;
-  countries={
-    IND:{
-      code:"IND",
-    name:"India",
-    states:{
-      TN:{
-        code:"TN",
-        name:"Tamil Nadu",
-        cities:[{
-          code:"MDU",
-          name:"Madurai"
-        },{
-          code:"CHN",
-          name:"Chennai"
-        }]
+  myForm: FormGroup;
+  countries = {
+    IND: {
+      code: 'IND',
+      name: 'India',
+      states: {
+        TN: {
+          code: 'TN',
+          name: 'Tamil Nadu',
+          cities: [
+            {
+              code: 'MDU',
+              name: 'Madurai',
+            },
+            {
+              code: 'CHN',
+              name: 'Chennai',
+            },
+          ],
+        },
+        KL: {
+          code: 'KL',
+          name: 'kerala',
+          cities: [
+            {
+              code: 'WND',
+              name: 'Wayanad',
+            },
+            {
+              code: 'KCH',
+              name: 'Kochin',
+            },
+          ],
+        },
       },
-      KL:{
-        code:"KL",
-        name:"kerala",
-        cities:[{
-          code:"WND",
-          name:"Wayanad"
-        },{
-          code:"KCH",
-          name:"Kochin"
-        }]
-      }
-    }
     },
-    DB:{
-      code:"DB",
-    name:"Dubai",
-    states:{
-      DM:{
-        code:"DM",
-        name:"Dubai Main Road",
-        cities:[{
-          code:"VK",
-          name:"Vivekandar theru"
-        }]
+    DB: {
+      code: 'DB',
+      name: 'Dubai',
+      states: {
+        DM: {
+          code: 'DM',
+          name: 'Dubai Main Road',
+          cities: [
+            {
+              code: 'VK',
+              name: 'Vivekandar theru',
+            },
+          ],
+        },
+        DK: {
+          code: 'DK',
+          name: 'Dubai kuruku theru',
+          cities: [
+            {
+              code: 'BS',
+              name: 'City 1',
+            },
+          ],
+        },
       },
-      DK:{
-        code:"DK",
-        name:"Dubai kuruku theru",
-        cities:[{
-          code:"BS",
-          name:"City 1"
-        }]
-      }
-    }
-    }
-  }
-  // countryList=[];
-  stateList=[];
-  cityList=[];
-  constructor(){
-    this.myForm=new FormGroup({
-      name:new FormControl('',Validators.required),
-      email:new FormControl('',[Validators.required,Validators.email]),
-      pass:new FormControl('',Validators.required),
-      cnfrm_pass:new FormControl('',Validators.required),
-      country:new FormControl('',Validators.required),
-      state:new FormControl('',Validators.required),
-      city:new FormControl('',Validators.required),
-      gender:new FormControl('',Validators.required),
-      marital_status:new FormControl('',Validators.required),
-      favFood:new FormControl('',Validators.required),
-      favcolor:new FormControl('',Validators.required)
-    })
-    this.myForm.get('country').valueChanges.subscribe((data)=>{
-      // console.log(data);
-      this.stateList=Object.keys(this.countries[data].states).map((item)=>{
-        return this.countries[data].states[item];
-      });
-      })
-    this.myForm.get('state').valueChanges.subscribe((data)=>{
-      // console.log(this.stateList,data);
-      // console.log(this.countries[this.myForm.get('country').value]);
-      this.cityList=this.countries[this.myForm.get('country').value]["states"][data]["cities"];
-      // console.log(this.cityList);
-      })
+    },
   };
-  countryList=Object.keys(this.countries);
+  countryList=[];
+  stateList = [];
+  cityList = [];
+      i:string;
+  constructor(private fb: FormBuilder) {
+    this.countryList = Object.keys(this.countries);
+    // console.log(this.countryList);
+    // this.myForm=new FormGroup({
+    //   name:new FormControl('',Validators.required),
+    //   email:new FormControl('',[Validators.required,Validators.email]),
+    //   pass:new FormControl('',Validators.required),
+    //   cnfrm_pass:new FormControl('',Validators.required),
+    //   country:new FormControl('',Validators.required),
+    //   state:new FormControl('',Validators.required),
+    //   city:new FormControl('',Validators.required),
+    //   gender:new FormControl('',Validators.required),
+    //   marital_status:new FormControl('',Validators.required),
+    //   favFood:new FormControl('',Validators.required),
+    //   favcolor:new FormControl('',Validators.required)
+    // })
+    this.myForm = this.fb.group({
+      name: this.fb.control('', Validators.required),
+      email: this.fb.control('', [Validators.required, Validators.email]),
+      pass: this.fb.control('', Validators.required),
+      cnfrm_pass: this.fb.control('', Validators.required),
+      address: this.fb.array([this.fb.group({
+        country: this.fb.control('', Validators.required),
+        state: this.fb.control('', Validators.required),
+        city: this.fb.control('', Validators.required),
+        add1:this.fb.control('', Validators.required),
+        add2:this.fb.control('', Validators.required),
+        zipCode:this.fb.control('', Validators.required)
+      }),
+      this.fb.group({
+        country:this.fb.control('', Validators.required),
+        state: this.fb.control('', Validators.required),
+        city: this.fb.control('', Validators.required),
+        add1:this.fb.control('', Validators.required),
+        add2:this.fb.control('', Validators.required),
+        zipCode:this.fb.control('', Validators.required)
+      })]),
+      gender: this.fb.control('', Validators.required),
+      marital_status: this.fb.control('', Validators.required),
+      favFood: this.fb.control('', Validators.required),
+      favcolor: this.fb.control('', Validators.required),
 
-  onSubmit(){
-    if(this.myForm.valid){
-      console.log(this.myForm.value);
-      alert("Form Submitted successfully");
+    });
+    // console.log(this.myForm.get('address').get('0'))
+    for(let  i in this.myForm.get('address').value){
+      this.myForm.get('address').get(i).get('country').valueChanges.subscribe((data) => {
+        this.stateList[i] = Object.keys(this.countries[data].states).map((item) => {
+          return this.countries[data].states[item];
+        });
+      });
     }
-    else{
-      alert("Fill the form properly and try re submitting");
+    for(let i in this.myForm.get('address').value){
+      // console.log( this.myForm.get('address').get(i).get('state'))
+      this.myForm.get('address').get(i).get('state').valueChanges.subscribe((data) => {
+        this.cityList[i] = this.countries[this.myForm.get('address').get(i).get('country').value][
+          'states'
+        ][data]['cities'];
+        // console.log(this.cityList);
+      });
+    }
+    
+  }
+
+  onSubmit() {
+    if (this.myForm.valid) {
+      console.log(this.myForm.value);
+      alert('Form Submitted successfully');
+    } else {
+      alert('Fill the form properly and try re submitting');
     }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {ReactiveFormsModule,FormsModule} from '@angular/forms'
+import {StringCastPipe} from '../app/appstring.pipe'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StringCastPipe
   ],
   imports: [
     BrowserModule,
